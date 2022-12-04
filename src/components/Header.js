6096ff5ba1dbce5ec05117dd2941aba0c926c4a5
Cristiano Dalbem <ninja.dalbem@gmail.com>
2022-12-04 03:04:54 -0300

    Adds back "back" button on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -53,7 +53,7 @@ class Header extends React.Component {
         
             <div className={`${isHome && 'f7 dn db-ns'}`}>
               { 
-                isHome && 
+                isHome ? 
                   <Scrollspy
                     items={sectionsSlugs}
                     componentTag="div"
@@ -72,15 +72,15 @@ class Header extends React.Component {
                       ))
                     }
                   </Scrollspy>
-                  // :
-                  // <Link to="/" className="link dim fw6 orange">
-                  //   <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'
-                  //     fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round'
-                  //     strokeLinejoin='round' className='feather feather-corner-left-up'>
-                  //     <polyline points='14 9 9 4 4 9' />
-                  //     <path d='M20 20h-7a4 4 0 0 1-4-4V4' />
-                  //   </svg> Back
-                  // </Link>
+                  :
+                  <Link to="/" className="link dim fw6 orange">
+                    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'
+                      fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round'
+                      strokeLinejoin='round' className='feather feather-corner-left-up'>
+                      <polyline points='14 9 9 4 4 9' />
+                      <path d='M20 20h-7a4 4 0 0 1-4-4V4' />
+                    </svg> Back
+                  </Link>
               }
             </div>
           </Fade>
