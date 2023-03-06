4275a41679570eb39a28f738e247927273c7cdf8
cmdalbem <cristiano.dalbem@gmail.com>
2023-03-06 11:52:50 -0300

    Extra padding to back button

--- src/components/Header.js
+++ src/components/Header.js
@@ -73,7 +73,7 @@ class Header extends React.Component {
                     }
                   </Scrollspy>
                   :
-                  <Link to="/" className="link dim fw6 orange">
+                  <Link to="/" className="link dim fw6 orange pa2">
                     <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'
                       fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round'
                       strokeLinejoin='round' className='feather feather-corner-left-up'>
