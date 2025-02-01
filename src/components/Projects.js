11b217cc890bbf30c08e10372d177f3bd23f564d
Cristiano Dalbem <cristiano.dalbem@gmail.com>
2025-02-01 16:27:05 +0100

    Adjusts layout of projects thumbnails on home

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,7 @@ class Projects extends React.Component {
         const posts = this.props.posts;
         const mini = this.props.mini;
 
-        let classes = mini ? "w-25-ns w-25-m w-50 " : "w-50-ns w-50-m w-100 ";
+        let classes = mini ? "w-third-ns w-third-m w-50 " : "w-100-ns w-100-m w-100 ";
         classes += "animatable mv4-ns mv3 pr4-ns pr3-m pr3";
 
         // const globalPadding = 'ph6-ns ph4-m ph3 ';
