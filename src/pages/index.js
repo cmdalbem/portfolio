b1acd63485802e2d0e2698c42c2687075dd95e0c
Cristiano Dalbem <cristiano.dalbem@gmail.com>
2025-06-20 11:54:55 +0200

    Small copy improvements in Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -74,8 +74,8 @@ class IndexPage extends React.Component {
                   : <AnimatedHeader
                       typography={titleTypography} 
                       rows={[
-                        'Trained in code, self-taught in',
-                        'design. For 10 years building impactful', 
+                        'Trained in code & self-taught in',
+                        'design, for 10 years building impactful', 
                         'digital products for work and for love.'
                   ]}/> 
                 }
@@ -122,12 +122,12 @@ class IndexPage extends React.Component {
             <div className="f2-ns f4 lh-title fw4 dark-gray measure">
               <Reveal effect="slideUp" duration={2000} big>
                   <p className="text-gradient-clip">
-                    With the projects I delivered, I impacted the lives of millions of people by <span className='ttext'>simplifying</span> their financial lives, <span className='ttext'>streamlining efficient and humane</span> customer support, <span className='ttext'>scaling</span> some of Brazil's largest e-commerces, and fostering urban cycling <span className='ttext'>making our cities more livable and sustainable</span>.
+                    With the products I shipped, I impacted the lives of millions of people by <span className='ttext'>simplifying</span> their financial lives, <span className='ttext'>streamlining efficient and humane</span> customer support, <span className='ttext'>scaling</span> some of Brazil's largest e-commerces, and fostering urban cycling <span className='ttext'>making cities more livable and sustainable</span>.
                   </p>
               </Reveal>
                <Reveal effect="slideUp" duration={2000} big>
                   <p className="text-gradient-clip">
-                    I'm passionate about solving complex problems by <span className='ttext'>coding and designing</span> user-centric, elegant and strategic solutions, from interfaces to systems. More than anything I'm a builder, passionate about <span className='ttext'>making ideas come to life</span>.
+                    I work solving complex problems by <span className='ttext'>coding and designing</span> user-centric, elegant and strategic solutions, from interfaces to systems. More than anything I'm a builder, passionate about <span className='ttext'>making ideas come to life</span>.
                   </p>
               </Reveal>
             </div>
