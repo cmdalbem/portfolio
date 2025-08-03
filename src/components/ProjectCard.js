27cc93de17823353727b04caaed23859158d8a08
Cristiano Dalbem <cristiano.dalbem@gmail.com>
2025-08-03 09:59:27 +0200

    Fix SVG attributes

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -30,7 +30,7 @@ class ProjectCard extends React.Component {
                 onMouseEnter={() => this.setState({hover: isMobile ? false : true})}
                 onMouseLeave={() => this.setState({hover: isMobile ? false : false})}
             >
-                <div className={"db br3 project-card--cover overflow-hidden " + (mini ? "w-100-ns w-50 h-100 aspect-ratio-ns aspect-ratio--16x9-ns" : "w-100 aspect-ratio aspect-ratio--16x9")}>
+                <div className={"db project-card--cover overflow-hidden " + (mini ? "w-100-ns w-50 h-100 aspect-ratio-ns aspect-ratio--16x9-ns" : "w-100 aspect-ratio aspect-ratio--16x9")}>
                     <Img
                         fluid={post.frontmatter.cover.childImageSharp.fluid}
                         className={`w-100  ${isShowHover ? 'dn' : ''}`}
@@ -53,7 +53,7 @@ class ProjectCard extends React.Component {
                             {
                                 post.frontmatter.isPasswordProtected &&
                                 <span className='ml2 silver'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-lock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"/><path d="M7 11V7a5 5 0 0 1 10 0v4"/></svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-lock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"/><path d="M7 11V7a5 5 0 0 1 10 0v4"/></svg>
                                 </span>
                             }
                         </div>
@@ -68,7 +68,7 @@ class ProjectCard extends React.Component {
 
                     {
                         post.frontmatter.tags && 
-                        <div className="flex flex-row mt2 nl1">
+                        <div className="flex flex-row mt3 nl1">
                             {
                                 post.frontmatter.tags.map(function(t) {
                                     t = capitalize(t);
--- src/components/markdownComponents.js
+++ src/components/markdownComponents.js
@@ -67,7 +67,7 @@ class ProjectPasswordInput extends React.Component {
         return (
             <div className="mv7-ns mv5 center tc mw6-ns bg-near-white pa4 br2">
                 <div className="f2 mv3 br-100 pa1 h3 w3 center bg-white-70">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"/><path d="M7 11V7a5 5 0 0 1 10 0v4"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-lock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"/><path d="M7 11V7a5 5 0 0 1 10 0v4"/></svg>
                 </div>
                 <div className='f5 gray'>
                     <div>This project is password-protected.</div>
